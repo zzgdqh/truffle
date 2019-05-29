b9f40959225dbbce875e193bcf34922f7673f4f9
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-29 14:56:12 -0700

    misc bootstrap.js

--- packages/truffle-contract/lib/contract/bootstrap.js
+++ packages/truffle-contract/lib/contract/bootstrap.js
@@ -3,12 +3,12 @@ const execute = require("../execute");
 module.exports = fn => {
   // Add our static methods
   // Add something here about excluding send, privately defined methods
-  Object.keys(fn._constructorMethods).forEach(function(key) {
+  Object.keys(fn._constructorMethods).forEach(key => {
     fn[key] = fn._constructorMethods[key].bind(fn);
   });
 
   // Add our properties.
-  Object.keys(fn._properties).forEach(function(key) {
+  Object.keys(fn._properties).forEach(key => {
     fn.addProp(key, fn._properties[key]);
   });
 
