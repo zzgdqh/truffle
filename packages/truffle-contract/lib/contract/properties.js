2f7ebbfc65e4c04acab8714a49b4cbafad5a8811
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-05-29 14:59:06 -0700

    es6 properties.js

--- packages/truffle-contract/lib/contract/properties.js
+++ packages/truffle-contract/lib/contract/properties.js
@@ -3,60 +3,60 @@ const web3Utils = require("web3-utils");
 
 module.exports = {
   contract_name: {
-    get: function() {
+    get() {
       return this.contractName;
     },
-    set: function(val) {
+    set(val) {
       this.contractName = val;
     }
   },
   contractName: {
-    get: function() {
+    get() {
       return this._json.contractName || "Contract";
     },
-    set: function(val) {
+    set(val) {
       this._json.contractName = val;
     }
   },
 
   gasMultiplier: {
-    get: function() {
+    get() {
       if (this._json.gasMultiplier === undefined) {
         this._json.gasMultiplier = 1.25;
       }
       return this._json.gasMultiplier;
     },
-    set: function(val) {
+    set(val) {
       this._json.gasMultiplier = val;
     }
   },
   timeoutBlocks: {
-    get: function() {
+    get() {
       return this._json.timeoutBlocks;
     },
-    set: function(val) {
+    set(val) {
       this._json.timeoutBlocks = val;
     }
   },
   autoGas: {
-    get: function() {
+    get() {
       if (this._json.autoGas === undefined) {
         this._json.autoGas = true;
       }
       return this._json.autoGas;
     },
-    set: function(val) {
+    set(val) {
       this._json.autoGas = val;
     }
   },
   numberFormat: {
-    get: function() {
+    get() {
       if (this._json.numberFormat === undefined) {
         this._json.numberFormat = "BN";
       }
       return this._json.numberFormat;
     },
-    set: function(val) {
+    set(val) {
       const allowedFormats = ["BigNumber", "BN", "String"];
 
       const msg =
@@ -69,46 +69,41 @@ module.exports = {
     }
   },
   abi: {
-    get: function() {
+    get() {
       return this._json.abi;
     },
-    set: function(val) {
+    set(val) {
       this._json.abi = val;
     }
   },
-  metadata: function() {
+  metadata() {
     return this._json.metadata;
   },
-  network: function() {
-    var network_id = this.network_id;
+  network() {
+    const network_id = this.network_id;
 
     if (network_id == null) {
-      var error =
-        this.contractName +
-        " has no network id set, cannot lookup artifact data." +
-        " Either set the network manually using " +
-        this.contractName +
-        ".setNetwork(), run " +
-        this.contractName +
-        ".detectNetwork(), or use new()," +
-        " at() or deployed() as a thenable which will detect the network automatically.";
+      var error = `${
+        this.contractName
+      } has no network id set, cannot lookup artifact data. Either set the network manually using ${
+        this.contractName
+      }.setNetwork(), run ${
+        this.contractName
+      }.detectNetwork(), or use new(), at() or deployed() as a thenable which will detect the network automatically.`;
 
       throw new Error(error);
     }
 
     // TODO: this might be bad; setting a value on a get.
     if (this._json.networks[network_id] == null) {
-      var error =
-        this.contractName +
-        " has no network configuration" +
-        " for its current network id (" +
-        network_id +
-        ").";
+      var error = `${
+        this.contractName
+      } has no network configuration for its current network id (${network_id}).`;
 
       throw new Error(error);
     }
 
-    var returnVal = this._json.networks[network_id];
+    const returnVal = this._json.networks[network_id];
 
     // Normalize output
     if (returnVal.links == null) {
@@ -121,41 +116,37 @@ module.exports = {
 
     return returnVal;
   },
-  networks: function() {
+  networks() {
     return this._json.networks;
   },
   address: {
-    get: function() {
-      var address = this.network.address;
+    get() {
+      const address = this.network.address;
 
       if (address == null) {
-        var error =
-          "Cannot find deployed address: " +
-          this.contractName +
-          " not deployed or address not set.";
+        const error = `Cannot find deployed address: ${
+          this.contractName
+        } not deployed or address not set.`;
         throw new Error(error);
       }
 
       return address;
     },
-    set: function(val) {
+    set(val) {
       if (val == null) {
-        throw new Error("Cannot set deployed address; malformed value: " + val);
+        throw new Error(`Cannot set deployed address; malformed value: ${val}`);
       }
 
-      var network_id = this.network_id;
+      const network_id = this.network_id;
 
       if (network_id == null) {
-        var error =
-          this.contractName +
-          " has no network id set, cannot lookup artifact data." +
-          " Either set the network manually using " +
-          this.contractName +
-          ".setNetwork(), run " +
-          this.contractName +
-          ".detectNetwork()," +
-          " or use new(), at() or deployed() as a thenable which will" +
-          " detect the network automatically.";
+        const error = `${
+          this.contractName
+        } has no network id set, cannot lookup artifact data. Either set the network manually using ${
+          this.contractName
+        }.setNetwork(), run ${
+          this.contractName
+        }.detectNetwork(), or use new(), at() or deployed() as a thenable which will detect the network automatically.`;
 
         throw new Error(error);
       }
@@ -173,25 +164,22 @@ module.exports = {
     }
   },
   transactionHash: {
-    get: function() {
+    get() {
       return this.network.transactionHash;
     },
-    set: function(val) {
+    set(val) {
       this.network.transactionHash = val;
     }
   },
-  links: function() {
+  links() {
     if (!this.network_id) {
-      var error =
-        this.contractName +
-        " has no network id set, cannot lookup artifact data." +
-        " Either set the network manually using " +
-        this.contractName +
-        ".setNetwork()," +
-        " run " +
-        this.contractName +
-        ".detectNetwork(), or use new(), at()" +
-        " or deployed() as a thenable which will detect the network automatically.";
+      const error = `${
+        this.contractName
+      } has no network id set, cannot lookup artifact data. Either set the network manually using ${
+        this.contractName
+      }.setNetwork(), run ${
+        this.contractName
+      }.detectNetwork(), or use new(), at() or deployed() as a thenable which will detect the network automatically.`;
 
       throw new Error(error);
     }
@@ -202,8 +190,8 @@ module.exports = {
 
     return this.network.links || {};
   },
-  events: function() {
-    var events;
+  events() {
+    let events;
 
     if (this._json.networks[this.network_id] == null) {
       events = {};
@@ -212,18 +200,18 @@ module.exports = {
     }
 
     // Merge abi events with whatever's returned.
-    var abi = this.abi;
+    const abi = this.abi;
 
-    abi.forEach(function(item) {
+    abi.forEach(item => {
       if (item.type !== "event") return;
 
       if (item.signature) {
         events[item.signature] = item;
       } else {
-        var signature = item.name + "(";
+        let signature = `${item.name}(`;
 
-        item.inputs.forEach(function(input, index) {
-          signature += input.type;
+        item.inputs.forEach(({ type }, index) => {
+          signature += type;
 
           if (index < item.inputs.length - 1) {
             signature += ",";
@@ -232,7 +220,7 @@ module.exports = {
 
         signature += ")";
 
-        var topic = web3Utils.keccak256(signature);
+        const topic = web3Utils.keccak256(signature);
 
         events[topic] = item;
       }
@@ -240,129 +228,129 @@ module.exports = {
 
     return events;
   },
-  binary: function() {
+  binary() {
     return utils.linkBytecode(this.bytecode, this.links);
   },
-  deployedBinary: function() {
+  deployedBinary() {
     return utils.linkBytecode(this.deployedBytecode, this.links);
   },
 
   // deprecated; use bytecode
   unlinked_binary: {
-    get: function() {
+    get() {
       return this.bytecode;
     },
-    set: function(val) {
+    set(val) {
       this.bytecode = val;
     }
   },
   // alias for unlinked_binary; unlinked_binary will eventually be deprecated
   bytecode: {
-    get: function() {
+    get() {
       return this._json.bytecode;
     },
-    set: function(val) {
+    set(val) {
       this._json.bytecode = val;
     }
   },
   deployedBytecode: {
-    get: function() {
-      var code = this._json.deployedBytecode;
+    get() {
+      let code = this._json.deployedBytecode;
 
       if (code.indexOf("0x") !== 0) {
-        code = "0x" + code;
+        code = `0x${code}`;
       }
 
       return code;
     },
-    set: function(val) {
-      var code = val;
+    set(val) {
+      let code = val;
 
       if (val.indexOf("0x") !== 0) {
-        code = "0x" + code;
+        code = `0x${code}`;
       }
 
       this._json.deployedBytecode = code;
     }
   },
   sourceMap: {
-    get: function() {
+    get() {
       return this._json.sourceMap;
     },
-    set: function(val) {
+    set(val) {
       this._json.sourceMap = val;
     }
   },
   deployedSourceMap: {
-    get: function() {
+    get() {
       return this._json.deployedSourceMap;
     },
-    set: function(val) {
+    set(val) {
       this._json.deployedSourceMap = val;
     }
   },
   source: {
-    get: function() {
+    get() {
       return this._json.source;
     },
-    set: function(val) {
+    set(val) {
       this._json.source = val;
     }
   },
   sourcePath: {
-    get: function() {
+    get() {
       return this._json.sourcePath;
     },
-    set: function(val) {
+    set(val) {
       this._json.sourcePath = val;
     }
   },
   legacyAST: {
-    get: function() {
+    get() {
       return this._json.legacyAST;
     },
-    set: function(val) {
+    set(val) {
       this._json.legacyAST = val;
     }
   },
   ast: {
-    get: function() {
+    get() {
       return this._json.ast;
     },
-    set: function(val) {
+    set(val) {
       this._json.ast = val;
     }
   },
   compiler: {
-    get: function() {
+    get() {
       return this._json.compiler;
     },
-    set: function(val) {
+    set(val) {
       this._json.compiler = val;
     }
   },
   // Deprecated
-  schema_version: function() {
+  schema_version() {
     return this.schemaVersion;
   },
-  schemaVersion: function() {
+  schemaVersion() {
     return this._json.schemaVersion;
   },
   // deprecated
-  updated_at: function() {
+  updated_at() {
     return this.updatedAt;
   },
-  updatedAt: function() {
+  updatedAt() {
     try {
       return this.network.updatedAt || this._json.updatedAt;
     } catch (e) {
       return this._json.updatedAt;
     }
   },
-  userdoc: function() {
+  userdoc() {
     return this._json.userdoc;
   },
-  devdoc: function() {
+  devdoc() {
     return this._json.devdoc;
   }
 };
