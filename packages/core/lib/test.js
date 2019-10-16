da4d87d667ea0115f2f1763bfe78a2621349436f
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-10-16 09:39:45 -0700

    swap out Profiler.updated for Common.updatedFiles (core/lib/test)

--- packages/core/lib/test.js
+++ packages/core/lib/test.js
@@ -1,4 +1,5 @@
 const Mocha = require("mocha");
+const { promisify } = require("util");
 const colors = require("colors");
 const chai = require("chai");
 const path = require("path");
@@ -15,7 +16,7 @@ const TestSource = require("./testing/testsource");
 const SolidityTest = require("./testing/soliditytest");
 const expect = require("@truffle/expect");
 const Migrate = require("@truffle/migrate");
-const Profiler = require("@truffle/compile-solidity/profiler");
+const { updatedFiles } = require("@truffle/compile-common");
 const originalrequire = require("original-require");
 const debug = require("debug")("lib:test");
 
@@ -165,7 +166,9 @@ const Test = {
     testResolver
   ) {
     const updated =
-      (await Profiler.updated(config.with({ resolver: testResolver }))) || [];
+      (await promisify(updatedFiles)(
+        config.with({ resolver: testResolver })
+      )) || [];
 
     const compileConfig = config.with({
       all: config.compileAll === true,
